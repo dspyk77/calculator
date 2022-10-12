2aef31a87b56878128a18325dc752f13c22ccc80
dspyk77 <dspyk77@gmail.com>
2022-10-12 13:32:34 -0500

    cleanup

--- Calculator1/Program.cs
+++ Calculator1/Program.cs
@@ -14,36 +14,26 @@ namespace Calculator
         {
             string firstNumber;
             string secondNumber;
-            
             bool repeatQ = true;
             bool isNum1, isNum2;
-            
-            
-            
-            
 
-                Console.WriteLine("Welcome to the calculator!\r\n Enter a number.");
+            Console.WriteLine("Welcome to the calculator!\r\n Enter a number.");
             // loops program until user if finished. ending each equation user is asked to continue or to press "x" to exit the program.
             while (repeatQ)
             {
-                
                 firstNumber = Console.ReadLine();
-
-                
 
                 if (isNum1 = int.TryParse(firstNumber, out int firstNumberParsed))
                 {
                     Console.WriteLine("Enter a second number.");
- 
                 }
                 else
                 {
-                    Console.WriteLine($" {firstNumber} is not a number... get out of here with the algebra shit. Try that again.");
+                    Console.WriteLine($" {firstNumber} is not a number... I'm a more.. Simple calculator. Try again?");
                     firstNumber = Console.ReadLine();
                 }
-                
+
                 secondNumber = Console.ReadLine();
-                
 
                 if (isNum2 = int.TryParse(secondNumber, out int secondNumberParsed))
                 {
@@ -51,16 +41,9 @@ namespace Calculator
                 }
                 else
                 {
-                    Console.WriteLine($" {secondNumber} is not a number... get out of here with the algebra shit. Try that again.");
+                    Console.WriteLine($" {secondNumber} is not a number... I'm a more.. Simple calculator. Try again?");
                     secondNumber = Console.ReadLine();
                 }
-
-
-
-
-
-
-
 
                 // checking to make sure user wont divide by zero. Currently working more as a warning. If user divideds by zero program still crashes. work in progress... 
 
@@ -72,52 +55,42 @@ namespace Calculator
                 else
                 {
                     Console.WriteLine("Enter a to add, s to subtract, m to multiply or d to divide");
-                    
                 }
-                
-                    
-                
 
                 switch (Console.ReadLine())
                 {
                     case "a":
-                        Console.WriteLine($"{firstNumber} plus {secondNumber} is " + (firstNumberParsed + secondNumberParsed) + ", You really needed a calculator for that?");
+                        Console.WriteLine($"{firstNumber} plus {secondNumber} is " + (firstNumberParsed + secondNumberParsed) + ", I love the simple equations!");
                         break;
 
                     case "s":
-                        Console.WriteLine($"{firstNumber} minus {secondNumber} is " + (firstNumberParsed - secondNumberParsed) + ", Alright alright you can put down your fingers now.");
+                        Console.WriteLine($"{firstNumber} minus {secondNumber} is " + (firstNumberParsed - secondNumberParsed) + ", Alright alright, you can put down your fingers now.");
                         break;
 
                     case "m":
-                        Console.WriteLine($"Boop beep bo boop {firstNumber} times {secondNumber} is " + (firstNumberParsed * secondNumberParsed) + " now get out of my program");
+                        Console.WriteLine($"Boop beep bo boop {firstNumber} times {secondNumber} is " + (firstNumberParsed * secondNumberParsed) + " ....whoa");
                         break;
 
                     case "d":
-                        Console.WriteLine($"{firstNumber} divided by {secondNumber} is " + (firstNumberParsed / secondNumberParsed) + " What ever happened to long divison?");
+                        Console.WriteLine($"{firstNumber} divided by {secondNumber} is " + (firstNumberParsed / secondNumberParsed) + " What ever happened to long divison? Calculators!");
                         break;
 
                     default:
-                        Console.WriteLine("You having a hard time reading bud?");
+                        Console.WriteLine("You know this is a calculator, right?");
                         break;
-
-
                 }
                 
-                    Console.WriteLine("Another equation? press x to exit");
-                
+                Console.WriteLine("Another equation? Keep on going! or just press x to exit");
+
                 switch (Console.ReadLine())
                 {
                     case "x":
                         repeatQ = false;
                         break;
                 }
-
             }
 
             Console.ReadKey();
-
-
-
         }
     }
 }
